91139a2a5f24fbdb518f576021e05c9e5f59257e
Валерий Мельников <MelnikoffVA@yandex.ru>
2017-03-09 14:27:48 +0700

    forgot to edit output function for flowers

--- Methods1OOP/Methods1OOP/functions.cpp
+++ Methods1OOP/Methods1OOP/functions.cpp
@@ -376,6 +376,24 @@ void flower::Out(ofstream &ofst)
 		ofst << "It's from a flower-bed." << endl;
 		break;
 	}
+	switch(habitat)
+	{
+	case DESERT:
+		ofst << "Its natural habitat is desert." << endl;
+		break;
+	case TUNDRA:
+		ofst << "Its natural habitat is tundra." << endl;
+		break;
+	case FOREST:
+		ofst << "Its natural habitat is forest." << endl;
+		break;
+	case JUNGLE:
+		ofst << "Its natural habitat is jungle." << endl;
+		break;
+	case FIELD:
+		ofst << "Its natural habitat is field." << endl;
+		break;
+	}
 	ofst << "Its name has " << consonant() << " consonants.\n";
 
 }
